a380593250baf1daaf3d4561a2d542f338d64867
nashville149 <milliamkarambu76@gmail.com>
2025-06-07 16:22:59 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Write your code in this file!
+// Define the currentUser variable
+const currentUser ="John Doe"; 
+
+// Define the welcomeMessage variable
+const welcomeMessage =  `Welcome to Flatbook, ${currentUser}!`; 
+
+
+
+//update the currentUser variable to be all uppercase
+const excitedWelcomeMessage =welcomeMessage.toUpperCase();
+
+// Define the shortGreeting variable
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
